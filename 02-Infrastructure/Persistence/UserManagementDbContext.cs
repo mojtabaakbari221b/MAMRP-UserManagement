e88de913565cdd521c206df91fb07ed6df2d75a6
Mojtaba Akbari <mojtaba.akbari.221b@gmail.com>
2024-10-26 12:41:27 +0330

    add db context and set project refrence in one level

---
+++ 02-Infrastructure/Persistence/UserManagementDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using UserManagement.Domain.Entities;
+
+
+namespace UserManagement.Infrastructure.Persistence
+{
+    internal class UserManagementDbContext
+    {
+        public DbSet<Member> Members { get; set; }
+        public DbSet<Menu> Menus { get; set; }
+        public DbSet<MenuGroup> MenuGroups { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Service> Service { get; set; }
+        public DbSet<ServiceGroup> ServiceGroups { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
