1d405a4240870a5aff698173c99c038d803a610d
Mojtaba Akbari <mojtaba.akbari.221b@gmail.com>
2024-10-30 14:00:52 +0330

    change model field name

--- 04-Api/Migrations/20241030103026_change is delete field name to is active.Designer.cs
+++ 04-Api/Migrations/20241030103026_change is delete field name to is active.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using UserManagement.Infrastructure.Persistence;
 
@@ -11,9 +12,11 @@ using UserManagement.Infrastructure.Persistence;
 namespace _04_Api.Migrations
 {
     [DbContext(typeof(UserManagementDbContext))]
-    partial class UserManagementDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241030103026_change is delete field name to is active")]
+    partial class changeisdeletefieldnametoisactive
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ 04-Api/Migrations/20241030103026_change is delete field name to is active.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _04_Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class changeisdeletefieldnametoisactive : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsDeleted",
+                table: "Users",
+                newName: "IsActive");
+
+            migrationBuilder.RenameColumn(
+                name: "IsDeleted",
+                table: "ServiceGroups",
+                newName: "IsActive");
+
+            migrationBuilder.RenameColumn(
+                name: "IsDeleted",
+                table: "Service",
+                newName: "IsActive");
+
+            migrationBuilder.RenameColumn(
+                name: "IsDeleted",
+                table: "Roles",
+                newName: "IsActive");
+
+            migrationBuilder.RenameColumn(
+                name: "IsDeleted",
+                table: "Menus",
+                newName: "IsActive");
+
+            migrationBuilder.RenameColumn(
+                name: "IsDeleted",
+                table: "MenuGroups",
+                newName: "IsActive");
+
+            migrationBuilder.RenameColumn(
+                name: "IsDeleted",
+                table: "Members",
+                newName: "IsActive");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsActive",
+                table: "Users",
+                newName: "IsDeleted");
+
+            migrationBuilder.RenameColumn(
+                name: "IsActive",
+                table: "ServiceGroups",
+                newName: "IsDeleted");
+
+            migrationBuilder.RenameColumn(
+                name: "IsActive",
+                table: "Service",
+                newName: "IsDeleted");
+
+            migrationBuilder.RenameColumn(
+                name: "IsActive",
+                table: "Roles",
+                newName: "IsDeleted");
+
+            migrationBuilder.RenameColumn(
+                name: "IsActive",
+                table: "Menus",
+                newName: "IsDeleted");
+
+            migrationBuilder.RenameColumn(
+                name: "IsActive",
+                table: "MenuGroups",
+                newName: "IsDeleted");
+
+            migrationBuilder.RenameColumn(
+                name: "IsActive",
+                table: "Members",
+                newName: "IsDeleted");
+        }
+    }
+}
