74623d83162950b5992313e98f21386a5c6e706c
mojtabaakbari <mojtaba.akbari.221b@gmail.com>
2024-11-07 15:53:48 +0330

    add service declaration class

---
+++ src/Share/Helper/ServiceDeclarationEnum.cs
@@ -0,0 +1,14 @@
+namespace Share.Helper;
+
+public sealed class ServiceDeclaration
+{
+    public const  int Register = 1000;
+    public const  int Login = 1001;
+    public const  int ChangeUserRole = 1002;
+    public const  int ChangeRoleSectionClaim = 1003;
+    public const  int GetAllSections = 1004;
+    public const  int GetByIdSection = 1005;
+    public const  int DeleteSection = 1006;
+    public const  int UpdateSection = 1007;
+    public const  int CreateSection = 1008;
+}
