c1c72d6fd98ad7ca13619b2d8332002789c09ab8
mojtabaakbari <mojtaba.akbari.221b@gmail.com>
2024-11-12 16:20:57 +0330

    adding PaginationResult.cs

---
+++ src/Application/Common/PaginationResult.cs
@@ -0,0 +1,17 @@
+namespace UserManagement.Application.Common;
+
+public class PaginationResult<T>
+{
+    public T Data { get; set; }
+    public int PageNumber { get; set; }
+    public int PageSize { get; set; }
+    public int TotalRecords { get; set; }
+
+    public PaginationResult(T data, int pageNumber, int pageSize, int totalRecords)
+    {
+        PageNumber = pageNumber;
+        PageSize = pageSize;
+        Data = data;
+        TotalRecords = totalRecords;
+    }
+}
