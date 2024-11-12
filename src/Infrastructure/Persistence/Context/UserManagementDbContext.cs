5223d1f3bc7e5bf7af3aafd610bfbc6d48cc8438
mojtabaakbari <mojtaba.akbari.221b@gmail.com>
2024-11-12 15:12:17 +0330

    seed declarated service into db context

--- src/Infrastructure/Persistence/Context/UserManagementDbContext.cs
+++ src/Infrastructure/Persistence/Context/UserManagementDbContext.cs
@@ -1,4 +1,6 @@
-﻿namespace UserManagement.Infrastructure.Persistence.Context;
+﻿using UserManagement.Infrastructure.ExternalServices.Identities.Helper;
+
+namespace UserManagement.Infrastructure.Persistence.Context;
 
 public class UserManagementDbContext(DbContextOptions<UserManagementDbContext> options)
     : IdentityDbContext<User, Role, Guid, UserClaim, UserRole, UserLogin, RoleClaim, UserToken>(options)
@@ -12,5 +14,26 @@ public class UserManagementDbContext(DbContextOptions<UserManagementDbContext> o
         base.OnModelCreating(modelBuilder);
 
         modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        
+        SeedingData(modelBuilder);
+    }
+
+    private void SeedingData(ModelBuilder modelBuilder)
+    {
+        var declaratedServices = ConstantRetriever.GetConstants(typeof(ServiceDeclaration));
+        foreach (var service in declaratedServices)
+        {
+            modelBuilder.Entity<Section>()
+                .HasData(
+                    new Section
+                    {
+                        Name = service.Name,
+                        Url = $"/{service.Name}",
+                        Description = service.Name,
+                        Code = service.Value,
+                        Type = SectionType.Service,
+                    }
+                );
+        }
     }
 }
