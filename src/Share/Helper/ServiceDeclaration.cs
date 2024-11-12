bf22033b0f1d67beeee3323e94c8685615c19f8c
mojtabaakbari <mojtaba.akbari.221b@gmail.com>
2024-11-12 15:11:58 +0330

    add new service declare in ServiceDeclaration.cs

--- src/Share/Helper/ServiceDeclaration.cs
+++ src/Share/Helper/ServiceDeclaration.cs
@@ -6,23 +6,20 @@ public sealed class ServiceDeclaration
     public const string Login = "010002";
     public const string ChangeUserRole = "010003";
     public const string ChangeRoleSectionClaim = "010004";
-    
     public const string CreateRole = "010005";
     public const string UpdateRole = "010006";
     public const string DeleteRole = "010007";
     public const string GetAllRole = "010008";
     public const string GetByIdRole = "010009";
-    
     public const string GetAllSections = "010009";
     public const string GetByIdSection = "010010";
     public const string DeleteSection = "010011";
     public const string UpdateSection = "010012";
     public const string CreateSection = "010013";
-    
-    
     public const string GetAllSectionGroup = "010014";
     public const string GetByIdSectionGroup = "010015";
     public const string DeleteSectionGroup = "010016";
     public const string UpdateSectionGroup = "010017";
     public const string CreateSectionGroup = "010018";
+    public const string ChangeUserSectionClaim = "010019";
 }
