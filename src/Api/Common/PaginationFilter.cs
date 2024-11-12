cedeb8e0a31a7e071506b3c626d152e625a2a07c
mojtabaakbari <mojtaba.akbari.221b@gmail.com>
2024-11-12 16:20:10 +0330

    add PaginationFilter.cs

---
+++ src/Api/Common/PaginationFilter.cs
@@ -0,0 +1,19 @@
+namespace UserManagement.Api.Common;
+
+public class PaginationFilter
+{
+    public int PageNumber { get; set; }
+    public int PageSize { get; set; }
+    private int MaxPageSize { get; set; } = 1000;
+    private int DefaultPageSize { get; set; } = 10;
+    public PaginationFilter()
+    {
+        this.PageNumber = 1;
+        this.PageSize = DefaultPageSize;
+    }
+    public PaginationFilter(int pageNumber, int pageSize)
+    {
+        this.PageNumber = pageNumber < 1 ? 1 : pageNumber;
+        this.PageSize = pageSize > MaxPageSize ? DefaultPageSize : pageSize;
+    }
+}
