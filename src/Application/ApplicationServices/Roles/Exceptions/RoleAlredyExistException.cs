8243c4e50682a232946e941785dc9ec4fec8dc3d
mmadkazem <mmadkzm5@gmail.com>
2024-11-13 06:39:22 +0330

    feature: add service code to exception

--- src/Application/ApplicationServices/Roles/Exceptions/RoleAlredyExistException.cs
+++ src/Application/ApplicationServices/Roles/Exceptions/RoleAlredyExistException.cs
@@ -1,6 +1,4 @@
 ﻿namespace UserManagement.Application.ApplicationServices.Roles.Exceptions;
 
-public sealed class RoleAlredyExistException() : MamrpBaseBadRequestException("این نقش از قبل  وجود دارد")
-{
-    
-}
+public sealed class RoleAlredyExistException()
+    : MamrpBaseBadRequestException("این نقش از قبل  وجود دارد", ServicesCode.UserManagement);
