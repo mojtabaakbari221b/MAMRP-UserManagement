aac023e724a2a2909eabe370ce9d6e8d55f9dba2
mmadkazem <mmadkzm5@gmail.com>
2024-11-13 06:45:39 +0330

    Feature: add DisplayName and Nullable in Code Filed Section.cs

--- src/Domain/Entities/Section.cs
+++ src/Domain/Entities/Section.cs
@@ -3,10 +3,11 @@
 public class Section : BaseEntity
 {      
     public required string Name { get; set; }
+    public required string DisplayName { get; set; }
     public required string Url { get; set; }
     public SectionGroup? Group { get; set; }
     public long GroupId { get; set; }
     public required string Description { get; set; }
-    public required string Code { get; set; }
+    public string? Code { get; set; }
     public SectionType Type { get; init; }
 }
