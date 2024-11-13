233ae592ef5b1e8b1eb4daa87f637aa3881c9a1c
mmadkazem <mmadkzm5@gmail.com>
2024-11-13 06:38:26 +0330

    Add GetAll user Logic

---
+++ src/Application/ApplicationServices/Account/Queries/GetById/GetAllUserQueryHandler.cs
@@ -0,0 +1,13 @@
+﻿namespace UserManagement.Application.ApplicationServices.Account.Queries.GetById;
+
+public sealed class GetAllUserQueryHandler(IUnitOfWork uow)
+    : IRequestHandler<GetAllUserQueryRequest, IEnumerable<GetUserQueryResponse>>
+{
+    private readonly IUnitOfWork _uow = uow;
+
+    public async Task<IEnumerable<GetUserQueryResponse>> Handle(GetAllUserQueryRequest request, CancellationToken token)
+    {
+        var responses = await _uow.Users.GetAll(request.PageNumber, request.PageSize, token);
+        return responses.Adapt<IEnumerable<GetUserQueryResponse>>();
+    }
+}
