f2ed9280dd68247092c3292570c3bca14579c5eb
mojtabaakbari <mojtaba.akbari.221b@gmail.com>
2024-11-13 15:38:49 +0330

    Feature: Add pagination and filtering for menu service

--- src/Application/ApplicationServices/Menus/Queries/GetAll/GetAllMenuQueryRequest.cs
+++ src/Application/ApplicationServices/Menus/Queries/GetAll/GetAllMenuQueryRequest.cs
@@ -3,5 +3,5 @@ using UserManagement.Application.ApplicationServices.Menus.Filterings;
 
 namespace UserManagement.Application.ApplicationServices.Menus.Queries.GetAll;
 
-public sealed record GetAllMenuQueryRequest(PaginationFilter Pagination, MenuFiltering Filtering) 
-    : IRequest<IEnumerable<MenuDto>>;
+public sealed record GetAllMenuQueryRequest(PaginationFilter Pagination, MenuFiltering? Filtering) 
+    : IRequest<PaginationResult<IEnumerable<MenuDto>>>;
