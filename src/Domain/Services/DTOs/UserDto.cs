934feac0a9f497486b492223e9d7b9fce6db313e
mmadkazem <mmadkzm5@gmail.com>
2024-11-13 06:50:09 +0330

    Feature: Add UserId and FirstName and FamilyName in UserDto.cs

--- src/Domain/Services/DTOs/UserDto.cs
+++ src/Domain/Services/DTOs/UserDto.cs
@@ -1,3 +1,9 @@
 namespace UserManagement.Domain.Services.DTOs;
 
-public record UserDto(Guid Id, string UserName, string Email, string SecurityStamp);
+public record UserDto(
+    Guid UserId,
+    string UserName,
+    string FirstName,
+    string FamilyName,
+    string Email,
+    string SecurityStamp);
