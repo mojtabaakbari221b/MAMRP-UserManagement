a414696fcce923165e0ed82c9f10bd2ea9037da2
mmadkazem <mmadkzm5@gmail.com>
2024-11-13 06:47:09 +0330

    Fix: Change List method in to GetAllService and GetAllMenu Method and Change name Add method to AddAsync

--- src/Domain/Repositories/ISectionRepository.cs
+++ src/Domain/Repositories/ISectionRepository.cs
@@ -1,15 +1,12 @@
-using System.Collections.Specialized;
-using Share.Helper;
-using UserManagement.Domain.Entities;
-
 namespace UserManagement.Domain.Repositories;
 
 public interface ISectionRepository
 {
-    Task<Section> Add(Section section);
+    Task<Section> AddAsync(Section section);
     void Delete(Section section);
     void Update(Section section);
     Task<Section?> FindAsync(long id, CancellationToken token = default);
-    Task<IEnumerable<IResponse>> List(CancellationToken token = default);
-    Task<IResponse?> GetById(long id, CancellationToken token = default);
+    Task<IEnumerable<IResponse>> GetAllServices(int pageNumber, int pageSize, CancellationToken token = default);
+    Task<IEnumerable<IResponse>> GetAllMenus(int pageNumber, int pageSize, CancellationToken token = default);
+    Task<IResponse?> GetByIdService(long id, CancellationToken token = default);
 }
