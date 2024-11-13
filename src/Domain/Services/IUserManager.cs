c10eb21c67ce28d2c9a7b0138f07b37fdcbe1c43
mmadkazem <mmadkzm5@gmail.com>
2024-11-13 06:51:13 +0330

    Feature: Add AnyAsync, Delete, Update, GetAll Method in IUserManager.cs

--- src/Domain/Services/IUserManager.cs
+++ src/Domain/Services/IUserManager.cs
@@ -4,8 +4,13 @@ public interface IUserManager
 {
     Task AddRoleAndTheirClaimsToUserAsync(UserDto userDto, RoleDto roleDto);
     Task<LoginResult> Login(string username, string password);
-    Task<RegisterLogin> Register(RegisterDto registerDto);
+    Task<RegisterResult> Register(RegisterDto registerDto);
     Task RemoveUserRolesAndUserClaimsAsync(Guid userId);
     Task<UserDto?> GetUserById(string id);
     Task SaveToken(TokenDto tokens);
+    Task<bool> AnyAsync(Guid userId, CancellationToken token = default);
+    Task<bool> AnyAsync(string userName, CancellationToken token = default);
+    Task Delete(Guid userId, CancellationToken token);
+    Task Update(UserDto userDto, CancellationToken token = default);
+    Task<IEnumerable<IResponse>> GetAll(int pageNumber, int pageSize, CancellationToken token = default);
 }
