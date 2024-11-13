bbbd230e8a4893ce2dcb9b2ee4a387c3e32637ec
mmadkazem <mmadkzm5@gmail.com>
2024-11-13 06:43:06 +0330

    Feature: add Query Logic for service

--- src/Application/ApplicationServices/Services/Queries/GetAll/GetAllServiceQueryHandler.cs
+++ src/Application/ApplicationServices/Services/Queries/GetAll/GetAllServiceQueryHandler.cs
@@ -7,7 +7,7 @@ public class GetAllServiceQueryHandler(IUnitOfWork uow)
 
     public async Task<IEnumerable<ServiceDto>> Handle(GetAllServiceQueryRequest request, CancellationToken token)
     {
-       var response =  await _uow.Sections.GetAllService(token);
+       var response =  await _uow.Sections.GetAllServices(request.PageNumber, request.PageSize, token);
        return response.Adapt<IEnumerable<ServiceDto>>();
     }
 }
--- src/Application/ApplicationServices/Services/Queries/GetById/GetServiceByIdQueryHandler.cs
+++ src/Application/ApplicationServices/Services/Queries/GetById/GetServiceByIdQueryHandler.cs
@@ -7,7 +7,7 @@ public class GetServiceByIdQueryHandler(IUnitOfWork uow)
 
     public async Task<ServiceDto> Handle(GetSectionByIdQueryRequest request, CancellationToken token)
     {
-        var response = await _uow.Sections.GetById(request.Id, token)
+        var response = await _uow.Sections.GetByIdService(request.Id, token)
                        ?? throw new ServiceNotFoundException();
 
         return response.Adapt<ServiceDto>();
