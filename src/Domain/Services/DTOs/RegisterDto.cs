eae4b2f3c54acbe05c43bf0f458d0386e3fc9344
mmadkazem <mmadkzm5@gmail.com>
2024-11-13 06:48:39 +0330

    Feature: add RegisterDto.cs and add fistName field in RegisterDto.cs

--- src/Domain/Services/DTOs/RegisterDto.cs
+++ src/Domain/Services/DTOs/RegisterDto.cs
@@ -1,5 +1,5 @@
 ﻿namespace UserManagement.Domain.Services.DTOs;
 
-public sealed record RegisterDto(string Username, string FamilyName, string Password);
+public sealed record RegisterDto(string Username, string FirstName, string FamilyName, string Password);
 
-public sealed record RegisterLogin(bool Successed);
+public sealed record RegisterResult(bool Successed);
