d5bc1cbfe0a9154b68cdf07ebb89d2a8a1e73696
mojtabaakbari <mojtaba.akbari.221b@gmail.com>
2024-11-17 15:19:09 +0330

    change interceptor declaration logic in db context

--- src/Infrastructure/Persistence/Context/UserManagementDbContext.cs
+++ src/Infrastructure/Persistence/Context/UserManagementDbContext.cs
@@ -1,17 +1,22 @@
 ﻿namespace UserManagement.Infrastructure.Persistence.Context;
 
-public class UserManagementDbContext(DbContextOptions<UserManagementDbContext> options)
-    : IdentityDbContext<User, Role, Guid, UserClaim, UserRole, UserLogin, RoleClaim, UserToken>(options)
+public class UserManagementDbContext(
+    DbContextOptions<UserManagementDbContext> options,
+    FillBaseEntityValuesOnUpdatingInterceptor onUpdatingInterceptor,
+    FillBaseEntityValuesOnCreatingInterceptor onCreatingInterceptor
+    ) : IdentityDbContext<User, Role, Guid, UserClaim, UserRole, UserLogin, RoleClaim, UserToken>(options)
 {
+    private readonly FillBaseEntityValuesOnCreatingInterceptor _onCreatingInterceptor = onCreatingInterceptor;
+    private readonly FillBaseEntityValuesOnUpdatingInterceptor _onUpdatingInterceptor = onUpdatingInterceptor;
     public DbSet<UserRefreshToken> UserRefreshTokens { get; set; }
     public DbSet<SectionGroup> SectionGroups { get; set; }
     public DbSet<Section> Sections { get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         base.OnConfiguring(optionsBuilder);
-        // optionsBuilder.AddInterceptors(new SoftDeleteInterceptor());
-        optionsBuilder.AddInterceptors(new FillBaseEntityValuesOnUpdatingInterceptor());
-        optionsBuilder.AddInterceptors(new FillBaseEntityValuesOnCreatingInterceptor());
+        optionsBuilder.AddInterceptors(new SoftDeleteInterceptor());
+        optionsBuilder.AddInterceptors(_onCreatingInterceptor);
+        optionsBuilder.AddInterceptors(_onUpdatingInterceptor);
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
